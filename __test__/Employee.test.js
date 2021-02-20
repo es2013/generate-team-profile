9b7afbf8c7e30f1687d62c1176ebefa141c01aef
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-02-19 20:49:53 -0800

    employee methods and tests passed

---
+++ __test__/Employee.test.js
@@ -0,0 +1,33 @@
+const { any } = require('expect');
+const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    
+    let employee = new Employee('Erika',12345, 'applesauce@abc123.com');
+    // Create a test that checks if the properties are strings or numbers
+    expect(employee.name).toEqual(expect.any(String))
+    // Create a test that checks if the `strength` and `hitpoints` are `numbers`
+    // YOUR CODE HERE
+    expect(employee.email).toEqual(expect.any(String))
+    
+  });
+
+  test('test if name is captured using getName', () => {
+    let employee = new Employee('Erika', 12345, 'applesauce@abc123.com');
+    expect(employee.getName()).toBe('Erika');
+  });
+
+  test('test if id is captured using getID', () => {
+    let employee = new Employee('Erika', 12345, 'applesauce@abc123.com');
+    expect(employee.getID()).toEqual(expect.any(Number));
+  });
+
+  test('test if email is captured using getEmail', () => {
+    let employee = new Employee('Erika', 12345, 'applesauce@abc123.com');
+    expect(employee.getEmail()).toEqual(expect.stringContaining('@'));
+  });
+s
+  test('checks if they are an employee', () => {
+    let employee = new Employee('Erika', 12345, 'applesauce@abc123.com');
+    expect(employee.getRole()).toBe('Employee');
+  });
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+//export the constructor functions
+const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
+const Intern = require('../lib/Intern');
+const Manager = require('../lib/Manager')
