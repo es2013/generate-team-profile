b2c41af0234c99357a88cabc3359debe93f3160c
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-02-20 16:05:06 -0800

    Engineer constructor function and passing tests

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,1 +1,19 @@
 const Engineer = require('../lib/Engineer');
+
+const Employee = require('../lib/Employee');
+const { test, expect } = require('@jest/globals');
+
+test('checks to see if github username is captured', () => {
+    let engineer = new Engineer('Erika', 12345, 'applesauce@abc123.com', 'github123');
+    expect(engineer.github).toBe('github123');
+});
+
+test('checks if they are Manager using getRole', () => {
+    let engineer = new Engineer('Erika', 12345, 'applesauce@abc123.com', 'github123');
+    expect(engineer.getRole()).toBe('Engineer');
+});
+
+test('Check to see if github username is captured using getGithub', () => {
+    let engineer = new Engineer('Erika', 12345, 'applesauce@abc123.com', 'github123');
+    expect(engineer.getGithub()).toBe('github123')
+});
